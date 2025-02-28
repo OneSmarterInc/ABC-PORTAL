2e813e7294f04ec953c646849953c3c1222bb223
OneSmarterInc <135138556+OneSmarterInc@users.noreply.github.com>
2025-02-28 09:54:27 +0530

    Update urls.py

--- ABC_PORTAL/ABC_PORTAL/portal/urls.py
+++ ABC_PORTAL/ABC_PORTAL/portal/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('update_member',views.UpdateMemberInfo.as_view()),
     path('get_recent_data',views.MostRecentDataView.as_view()),
     path('get_dependent_data',views.GetMemberAndDependentsView.as_view()),
-    path('get_daily_count',views.GetMemberCountView.as_view())
+    path('get_daily_count',views.GetMemberCountView.as_view()),
+    path('get_claims',views.GetTotalClaimsDataView.as_view()),
+    path('get_claim_no_data',views.GetClaimsDataUsingClaimNoView.as_view()),
+    path('get_claims_using_ssn',views.GetClaimsDataView),
 ]
